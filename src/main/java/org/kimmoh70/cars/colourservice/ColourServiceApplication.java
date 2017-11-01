300e988ffe1b5fe03e050eab7fd1f14ccc851f09
kimmoh70 <kimmoh70@gmail.com>
2017-11-01 21:42:57 +1000

    Initial

---
+++ src/main/java/org/kimmoh70/cars/colourservice/ColourServiceApplication.java
@@ -0,0 +1,12 @@
+package org.kimmoh70.cars.colourservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColourServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColourServiceApplication.class, args);
+	}
+}
